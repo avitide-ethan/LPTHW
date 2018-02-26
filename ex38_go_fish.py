48e76f5a4ae297e0749b9c92835476555a705c3f
edow <ethandow@gmail.com>
2018-02-26 14:23:16 -0500

    code clarification. enable score tracking.

--- ex38_go_fish.py
+++ ex38_go_fish.py
@@ -3,15 +3,7 @@ import collections
 
 
 def make_new_deck():
-    deck = []
-    for i in range(0, 52):
-        deck.append(i)
-        i += 1
-    return deck
-
-
-def shuffle_deck(deck):
-    random.shuffle(deck)
+    deck = [i for i in range(52)]
     return deck
 
 
@@ -22,11 +14,13 @@ def card_suit(card_number):
 
 
 def card_index_value_convert(card_number, switch):
+    """if switch true, convert from card index value to card value. if false, convert from card value to
+    a list of the four potential card index values"""
     card_value_options = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ')
     if switch:  # convert from card index value to card value
         card_value = card_number % 13
         return card_value_options[card_value]
-    if not switch:  # convert from card value to card index value. shit but it could be one of four.
+    if not switch:  # convert from card value to list of card index values
         z = card_value_options.index(card_number)
         potential_cards = [z + (13 * x) for x in range(0, 4)]
         return potential_cards
@@ -57,25 +51,17 @@ def start_game(num_players):
     """input number of players, outputs player_list, hand_list, and game_deck"""
     game_deck = make_new_deck()
     player_list = []
+    score_list = [None]*num_players
     hand_list = [None]*num_players
     # print(hand_list)
     for player in range(0, num_players):
         hand_list[player] = [None]
-    # print("\nHand list before any assignment is:", end=" ")
-    # print(hand_list)
-    for player in range(0, num_players):  # assign player_list
+        score_list[player] = 0
         player_list.append("Player " + player.__str__())
-    # print("Player list is: ", player_list)
-    for player in range(0, num_players):  # assign hands
-        # print("\nAssigning values for player {}".format(player))
         game_deck, hand_list = deal(game_deck, hand_list, player, 5)
-        # print(f"Hand list is {hand_list}")
-        # print(hand_list[player])
-        # print(hand_list)
-    print(player_list)
-    # print(card_value_check(hand_list[0], '2'))
-    print(hand_list)
-    return game_deck, player_list, hand_list
+    print("The players in the game are: {}".format(', '.join(player_list)))
+    # print(hand_list)
+    return game_deck, player_list, hand_list, score_list
 
 
 def card_value_check(cardlist, value):
@@ -87,9 +73,9 @@ def card_value_check(cardlist, value):
     return False
 
 
-def turn(game_deck, player_list, hand_list, current_player):
+def turn(game_deck, player_list, hand_list, current_player, score_list):
     """prompt for another player and a card value. perform card value check. if true, grab card and go again
-    if false, deal 1 card from deck check deck to see i9f no cards left. """
+    if false, deal 1 card from deck check deck to see if no cards left. """
     z = 1
     while z == 1:
         print(hand_list)
@@ -98,27 +84,20 @@ def turn(game_deck, player_list, hand_list, current_player):
         player_looking = int(input("Who are you trying to steal it from?\n>"))
         if card_value_check(hand_list[player_looking], card_looking):  # if they have the card
             card_steal(hand_list, current_player, player_looking, card_looking)
-            if four_of_a_kind_check(hand_list[current_player]):
-                pass
-            else:
-                pass
+            four_of_a_kind_check(hand_list[current_player], score_list, current_player)
         else:  # GO FISH
-            print("Nope, they don't have that card. Go fish!")
-            input("> ")
-            go_fish(game_deck, hand_list, current_player)
-            if four_of_a_kind_check(hand_list[current_player]):
-                pass
-            else:
-                pass
+            input("Nope, they don't have that card. Go fish!\n> ")
+            new_card = random.sample(game_deck, 1)[0]  # TODO:if they fished  and got card_looking, they go again
+            go_fish(game_deck, hand_list, current_player, new_card)
+            four_of_a_kind_check(hand_list[current_player], score_list, current_player)
             z = 2
-            # draw a card from the game deck
-    print("Player {}, your hand is: {}".format(current_player, hand_list[current_player]))
+    print("Player {}, your hand is: {}".format(current_player, hand_id(hand_list[current_player])))
 
 
 def card_steal(hand_list, current_player, player_looking, card_looking):
     print("You just got SNIPED for a {}".format(card_looking))
-    print("Therefore have the following cards: {}"
-          .format(card_index_value_convert(card_looking, False)))  # you have these card indices in your hand
+    # print("Therefore have the following cards: {}"
+    #       .format(card_index_value_convert(card_looking, False)))  # you have these card indices in your hand
     for potential_card_index in card_index_value_convert(card_looking, False):  # remove from hand
         if potential_card_index in hand_list[player_looking]:
             found_card = potential_card_index
@@ -131,15 +110,14 @@ def card_steal(hand_list, current_player, player_looking, card_looking):
     return hand_list
 
 
-def go_fish(game_deck, hand_list, current_player):
-    new_card = random.sample(game_deck, 1)[0]
+def go_fish(game_deck, hand_list, current_player, new_card):
     game_deck.remove(new_card)
     hand_list[current_player].append(new_card)
     print(f"You drew a {card_id(new_card)}")
     return hand_list
 
 
-def four_of_a_kind_check(cardlist):
+def four_of_a_kind_check(cardlist, score_list, current_player):
     """check if a cardlist has four of a kind"""
     card_value_list = []
     for item in cardlist:   # determine card values for cards in cardlist and put into card_value_list
@@ -148,14 +126,15 @@ def four_of_a_kind_check(cardlist):
     for key in counter.keys():
         if counter[key] == 4:  # key is the value that appears 4 times
             four_card_list = card_index_value_convert(key, False)
-            print("Bitch, you got four {}'s!!".format(key))
+            print("Damn son, you got four {}'s!!".format(key))
             for card in four_card_list:
-                print(card)
-                print(cardlist)
                 cardlist.remove(card)
             print("Done removing cards")
             print(cardlist)
-            return cardlist
+            print(score_list)
+            score_list[current_player] += 1
+            print(f"tempscore is {tempscore}")
+            return cardlist, score_list
         else:
             pass
     return False  # four of a kind if it's true
@@ -181,14 +160,14 @@ def four_of_a_kind_check(cardlist):
 # # print(hand_id(hand1))
 # print(four_of_a_kind_check(hand1))
 
-
-global_deck, global_player_list, global_hand_list = start_game(5)
+participants = int(input("So. You want to play Go Fish. Interesting.\nHow many people playing?\n> "))
+global_deck, global_player_list, global_hand_list, global_score_list = start_game(participants)
 # print(global_deck)
-# # print(global_player_list)
-# global_deck = [0, 4, 5, 12, 14, 17, 18, 20, 22, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 40, 42, 43, 44,
-#   45, 47, 48, 49]
-# global_hand_list = [[1], [15, 28, 41, 2], [25, 30, 20, 42, 34], [2, 26, 23, 50, 35], [43, 4, 8, 40, 22]]
+# print(global_player_list)
+print(global_score_list)
+global_deck = [0, 4, 5, 12, 14, 17, 18, 20, 22, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 40, 42, 43, 44, 45, 47, 48, 49]
+global_hand_list = [[1], [15, 28, 41, 2], [25, 30, 20, 42, 34], [2, 26, 23, 50, 35], [43, 4, 8, 40, 22]]
 
-turn(global_deck, global_player_list, global_hand_list, 0)
+turn(global_deck, global_player_list, global_hand_list, 0, global_score_list)
 
 print("Done")
